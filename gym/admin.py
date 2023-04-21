1966b05ad0387327c8a6c861fbc140466c25d916
priyalaxmi99 <49188926+priyalaxmi99@users.noreply.github.com>
2023-04-21 10:40:36 +0530

    updated

--- gym/admin.py
+++ gym/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Energy
+# from .models import Energy
 # Register your models here.
-admin.site.register(Energy)
+# admin.site.register(Energy)
 
-def __str__(self):
-	return self.user_name
+# def __str__(self):
+# 	return self.user_name
